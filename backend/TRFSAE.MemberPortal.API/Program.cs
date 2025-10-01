f54240a4700f5daed96101c63fb768c40af87200
Ricky Liang <liangricky7@gmail.com>
2025-09-30 21:53:30 -0500

    supabase set up

--- backend/TRFSAE.MemberPortal.API/Program.cs
+++ backend/TRFSAE.MemberPortal.API/Program.cs
@@ -1,15 +1,27 @@
-
+using Supabase;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// register Supabase client as a singleton for reuse across project
+builder.Services.AddSingleton(provider =>
 {
-    builder.Services.AddControllers();
-}
+    var options = new SupabaseOptions
+    {
+        AutoConnectRealtime = true,
+        AutoRefreshToken = true,
+    };
+
+    var url = builder.Configuration["SupabaseUrl"] ?? throw new InvalidOperationException("Supabase URL is not configured.");
+    var key = builder.Configuration["SupabaseKey"] ?? throw new InvalidOperationException("Supabase Key is not configured.");
+    return new Client(url, key, options);
+});
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
