35a1bded1deef1fa3d0d997896c11d4cb28844bd
Damien Vesper <ldamienvesper@gmail.com>
2025-10-08 00:25:33 -0500

    refactor (frontend): vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+import { defineConfig, type UserConfig } from "vite";
+import { resolve } from "path";
 
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+export default defineConfig(({ mode }) => {
+    const isDev = mode === "development";
+    const plugins: UserConfig["plugins"] = [
+        react(),
+        tailwindcss()
+    ];
+
+    if (!isDev) plugins.push(ViteImageOptimizer({ logStats: true }));
+
+    return {
+        build: {
+            cssMinify: "lightningcss"
+        },
+
+        css: {
+            devSourcemap: isDev
+        },
+
+        json: {
+            stringify: true
+        },
+
+        resolve: {
+            alias: {
+                "@": resolve(import.meta.dirname, "./src")
+            }
+        },
+
+        plugins,
+
+        logLevel: isDev ? "info" : "warn",
+        clearScreen: false
+    };
+});
