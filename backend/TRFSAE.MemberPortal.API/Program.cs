9b27d4bd00fc7cff40405c4fb060aa64a4b73459
Ricky Liang <liangricky7@gmail.com>
2025-10-09 10:44:55 -0500

    CORS setup

--- backend/TRFSAE.MemberPortal.API/Program.cs
+++ backend/TRFSAE.MemberPortal.API/Program.cs
@@ -37,7 +37,21 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
+// CORS stuff
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowReactApp");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
