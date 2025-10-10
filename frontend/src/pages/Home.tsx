b2ba3a0bbd9b827f047b120a5641a01491f89b1b
Damien Vesper <ldamienvesper@gmail.com>
2025-10-09 23:12:12 -0500

    chore (frontend): bump deps, lint

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import { Navbar06 } from "@/components/ui/NavBar/shadcn-io/navbar-06"
+import { Navbar06 } from "@/components/ui/NavBar/shadcn-io/navbar-06";
 
 export const Home = () => {
     return (
         <>
-        <Navbar06 />
-        <div className="p-5 flex justify-center">Home Page</div>
-         </>
-    )
-}
+            <Navbar06 />
+            <div className="p-5 flex justify-center">Home Page</div>
+        </>
+    );
+};
