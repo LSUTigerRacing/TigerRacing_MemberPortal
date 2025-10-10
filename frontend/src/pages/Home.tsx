9c59c26744350fafcc40e18815a1c661bafd7e8a
rachael-101906 <rachaelbergeron@icloud.com>
2025-10-09 22:15:17 -0500

    Created NavBar and Added Shadcn Components

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,10 @@
+import { Navbar06 } from "@/components/ui/NavBar/shadcn-io/navbar-06"
+
 export const Home = () => {
     return (
-        <div>Home Page</div>
-    );
-};
+        <>
+        <Navbar06 />
+        <div className="p-5 flex justify-center">Home Page</div>
+         </>
+    )
+}
