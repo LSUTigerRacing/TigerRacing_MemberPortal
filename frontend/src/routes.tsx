a7cc4f76a57e4ad5a3b8a67b5d496cc243bd5e30
Damien Vesper <ldamienvesper@gmail.com>
2025-10-10 15:16:05 -0500

    refactor (frontend): fix shadcn colors, change navbar, add splash page

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 // Public pages
 const Home = lazy(() => import("./pages/Home.tsx").then(module => ({ default: module.Home })));
+const Dashboard = lazy(() => import("./pages/Dashboard.tsx").then(module => ({ default: module.Dashboard })));
 
 export const AppRoutes = () => {
     return (
@@ -11,6 +12,9 @@ export const AppRoutes = () => {
                 <Routes>
                     {/* public pages */}
                     <Route path="/" element={<Home />} />
+
+                    <Route path="/dashboard" element={<Dashboard />} />
+
                     {/* if they try to go elsewhere */}
                     <Route path="*" element={<Navigate to="/" replace />} />
                 </Routes>
