5d9a983c4d150620b586a6f9355c12813a9e88e1
trinhhdinhh <trdinh321@gmail.com>
2025-10-14 15:49:48 -0500

    Trinh's Prototype

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 // Public pages
 const Home = lazy(() => import("./pages/Home.tsx").then(module => ({ default: module.Home })));
+const ProjectDashboard = lazy(() => import("./pages/admin-dashboard.tsx").then(module => ({ default: module.AdminDashboard })));
 
 export const AppRoutes = () => {
     return (
@@ -11,6 +12,7 @@ export const AppRoutes = () => {
                 <Routes>
                     {/* public pages */}
                     <Route path="/" element={<Home />} />
+                    <Route path="/adminprojects" element={<ProjectDashboard />} />
                     {/* if they try to go elsewhere */}
                     <Route path="*" element={<Navigate to="/" replace />} />
                 </Routes>
