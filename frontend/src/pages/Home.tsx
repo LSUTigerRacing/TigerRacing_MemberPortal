61e0c7f6496409fdee1222510c94bfb44de6defe
CardinTran <ctran65@lsu.edu>
2025-10-16 10:42:54 -0500

    Updated UI Path and Routes
    Updated the UI path in components.json. Added a Learn More Button. Adjusted the home to have a button that routes to the task portal.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,16 @@
 import { Navbar06 } from "@/components/ui/NavBar/shadcn-io/navbar-06";
-import { MemberTabs } from "./Task-portal/Kanban";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+    const navigate = useNavigate();
     return (
         <>
             <Navbar06 />
-            <MemberTabs />
+            <div className="flex h-screen justify-center items-center">
+                <button onClick={() => navigate("TaskPortal")} className="rounded-lg bg-gray-900 px-4 py-2 text-white">
+                    Go to Tasks
+                </button>
+            </div>
         </>
     );
 };
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,8 +1,10 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { MemberTabs } from "./pages/Task-portal/Kanban.tsx";
 
 // Public pages
 const Home = lazy(() => import("./pages/Home.tsx").then(module => ({ default: module.Home })));
+const TaskPortal = lazy(() => import("./pages/Task-portal/Kanban.tsx").then(module => ({ default: module.MemberTabs })));
 
 export const AppRoutes = () => {
     return (
@@ -11,6 +13,7 @@ export const AppRoutes = () => {
                 <Routes>
                     {/* public pages */}
                     <Route path="/" element={<Home />} />
+                    <Route path="TaskPortal" element={<MemberTabs />} />
                     {/* if they try to go elsewhere */}
                     <Route path="*" element={<Navigate to="/" replace />} />
                 </Routes>
