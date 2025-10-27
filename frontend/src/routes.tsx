29c638f362fc610e189488104f1a93e460544365
rachael-101906 <rachaelbergeron@icloud.com>
2025-10-26 22:47:50 -0500

    Fixed routes to handle pages.tsx

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 // Public pages
 const Home = lazy(() => import("./pages/Home.tsx").then(module => ({ default: module.Home })));
+const Pages = lazy(() => import("@/pages/adminPortal/Pages.tsx").then(module => ({ default: module.PagesView })));
 
 export const AppRoutes = () => {
     return (
@@ -11,6 +12,8 @@ export const AppRoutes = () => {
                 <Routes>
                     {/* public pages */}
                     <Route path="/" element={<Home />} />
+                    <Route path="/pages" element={<Pages />} />
+
                     {/* if they try to go elsewhere */}
                     <Route path="*" element={<Navigate to="/" replace />} />
                 </Routes>
