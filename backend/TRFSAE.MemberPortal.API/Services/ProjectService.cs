342f84389a8b1e37e071307aad3636715b9022be
YZ-8607 <yingz6479@gmail.com>
2025-11-05 08:37:49 -0600

    AssignProjectMethod
    Implemented a new method in ProjectService to delete existing user project otherwise create new user project. Created a new model: UserProject.cs and also added into ProjectControllers and IProjectService

--- backend/TRFSAE.MemberPortal.API/Services/ProjectService.cs
+++ backend/TRFSAE.MemberPortal.API/Services/ProjectService.cs
@@ -100,13 +100,63 @@ public class ProjectService : IProjectService
             .Insert(newProject);
 
             return true;
-        } catch (Exception ex)
+        }
+        catch (Exception ex)
         {
             Console.WriteLine($"Error creating project: {ex.Message}");
             return false;
         }
+
+    }
+
+
+    public async Task<bool> AssignProjectAsync(Guid userId, Guid projectId)
+    {
+        var response = await _supabaseClient
+            .From<UserProjectModel>()
+            .Where(x => x.UserId == userId && x.ProjectId == projectId)
+            .Get();
+
+        var exist = response.Models.FirstOrDefault();
+
+        if (exist != null)
+        {
+            await _supabaseClient
+                .From<UserProjectModel>()
+                .Where(x => x.UserId == userId && x.ProjectId == projectId)
+                .Delete();
+
+            return true;
+
+        }
+
+        var newUserProject = new UserProjectModel
+        {
+            UserId = userId,
+            ProjectId = projectId
+
+        };
+
+
+         try
+        {
+            await _supabaseClient
+                .From<UserProjectModel>()
+                .Insert(newUserProject);
+
+            return true;
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error creating user project: {ex.Message}");
+            return false;
+        }
+        
         
     }
+    
+
 
     
 
