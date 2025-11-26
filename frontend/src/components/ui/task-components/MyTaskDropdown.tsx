b5dde777f8bc0f3530842ea2531f2b93947a528c
CardinTran <ctran65@lsu.edu>
2025-11-26 13:46:22 -0600

    My Task Tab Sidebar
    Dropdown for filtering options. Displays cards for selection to filter the task table beside it.

--- frontend/src/components/ui/task-components/MyTaskDropdown.tsx
+++ frontend/src/components/ui/task-components/MyTaskDropdown.tsx
@@ -1,4 +1,7 @@
-import { Button } from "@/components/ui/shadcn-components/button"
+// MyTaskContent Filter Dropdown component
+// Handles different selections for filtering
+
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -6,8 +9,8 @@ import {
   DropdownMenuTrigger,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
-} from "@/components/ui//shadcn-components/dropdown-menu"
-import{ LucideChevronDown } from "lucide-react"
+} from "@/components/ui/dropdown-menu";
+import { LucideChevronDown } from "lucide-react";
 
 interface FilterDropDownProps {
   onValueChange: (value: string) => void;
@@ -15,26 +18,37 @@ interface FilterDropDownProps {
 }
 
 export function FilterDropDown({ onValueChange, selectedValue }: FilterDropDownProps) {
-
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild className="">
+      <DropdownMenuTrigger asChild className="bg-white cursor-pointer">
         <Button variant="ghost" className="">
-          {selectedValue} 
+          {selectedValue}
           <LucideChevronDown />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-45 bg-white" align="start">
         <DropdownMenuLabel>Slice by</DropdownMenuLabel>
         <DropdownMenuRadioGroup value={selectedValue || ""} onValueChange={onValueChange}>
-          <DropdownMenuRadioItem value="Overall">Overall</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="Assignee">Assignee</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="Status">Status</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="Priority">Priority</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="Start Date">Start Date</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="End Date">End Date</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="Status" className="cursor-pointer">
+            Status
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="Assignee" className="cursor-pointer">
+            Assignee
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="Priority" className="cursor-pointer">
+            Priority
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="Start Date" className="cursor-pointer">
+            Start Date
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="End Date" className="cursor-pointer">
+            End Date
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="Label" className="cursor-pointer">
+            Label
+          </DropdownMenuRadioItem>
         </DropdownMenuRadioGroup>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
