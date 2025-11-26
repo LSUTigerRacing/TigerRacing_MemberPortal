06e4aeeea74f64104e3cdb330a4e15c20ab9d6d2
CardinTran <ctran65@lsu.edu>
2025-11-26 13:44:50 -0600

    Learn More
    Learn More Button. Shows Dialog for Project Description.

--- frontend/src/components/ui/task-components/LearnMore.tsx
+++ frontend/src/components/ui/task-components/LearnMore.tsx
@@ -1,4 +1,7 @@
-import { Button } from '@/components/ui/shadcn-components/button';
+// Learn More Button
+// Shows Dialog for Project Description
+
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogClose,
@@ -7,37 +10,38 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/shadcn-components/dialog"
-import { Separator } from "@/components/ui/shadcn-components/separator"
+} from "@/components/ui/dialog";
+import { Separator } from "@/components/ui/separator";
 
-const dummyText = "Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem"
+const dummyText =
+  "Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem";
 
 export function LearnMore() {
   return (
     <div>
       <Dialog>
-        <DialogTrigger>
-          <Button variant="outline" className="">
+        <DialogTrigger asChild>
+          <Button variant="outline" className="cursor-pointer">
             Learn More
           </Button>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
+        <DialogContent aria-describedby={undefined} className="sm:max-w-[425px]">
           <DialogHeader>
             <DialogTitle>Learn More</DialogTitle>
             <div className="pt-2">
-                <Separator />
+              <Separator />
             </div>
           </DialogHeader>
-          <div className="grid gap-4">
-            {dummyText}
-          </div>
+          <div className="grid gap-4">{dummyText}</div>
           <DialogFooter>
             <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
+              <Button variant="outline" className="cursor-pointer">
+                Cancel
+              </Button>
             </DialogClose>
           </DialogFooter>
         </DialogContent>
       </Dialog>
     </div>
-  )
+  );
 }
