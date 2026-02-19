f2e6349d80a591c5b56fd8c89bdb853dc82346f7
Ricky Liang <liangricky7@gmail.com>
2026-02-18 22:14:37 -0600

    feat (backend): remove google sheets service

--- backend/TRFSAE.MemberPortal.API/Program.cs
+++ backend/TRFSAE.MemberPortal.API/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddScoped<IRoleService, RoleService>();
     builder.Services.AddScoped<ITaskService, TaskService>();
     builder.Services.AddScoped<IProjectService, ProjectService>();
-    builder.Services.AddScoped<IGoogleSheetsService, GoogleSheetsService>();
+    // builder.Services.AddScoped<IGoogleSheetsService, GoogleSheetsService>();
 }
 
 // register Supabase client as scoped for reuse across project
